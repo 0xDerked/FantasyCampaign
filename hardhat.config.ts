657fbf6a062eb0061ec39b3baf50cec24d6dc019
Jof <github@jofarnold.com>
2021-11-22 12:41:35 +0000

    Fix linting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,32 @@
-import { config as dotenvConfig } from 'dotenv';
-import { resolve } from 'path';
-dotenvConfig({ path: resolve(__dirname, './.env') });
+import { config as dotenvConfig } from "dotenv";
+import { resolve } from "path";
+dotenvConfig({ path: resolve(__dirname, "./.env") });
 
-import { HardhatUserConfig } from 'hardhat/types';
-import { NetworkUserConfig } from 'hardhat/types';
+import { HardhatUserConfig } from "hardhat/types";
 
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
 
 const config: HardhatUserConfig = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   solidity: {
-	  version: '0.8.0',
-	  settings: {
-		  optimizer: {
-			  enabled: true,
-			  runs:1000,
-		  }
-	  }
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
   },
   networks: {
     mumbai: {
       url: process.env.MUMBAI_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY || ''],
+      accounts: [process.env.PRIVATE_KEY || ""],
     },
   },
   typechain: {
-    outDir: 'typechain',
-    target: 'ethers-v5',
+    outDir: "typechain",
+    target: "ethers-v5",
   },
 };
 
