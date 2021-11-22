0c3ec0b85e0937f61bba3f914edcc15b0431f407
Jof <github@jofarnold.com>
2021-11-22 12:57:42 +0000

    Update hh config and fix linting

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { config as dotenvConfig } from "dotenv";
 import { resolve } from "path";
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
-import { HardhatUserConfig } from "hardhat/types";
+import { HardhatUserConfig, HttpNetworkConfig } from "hardhat/types";
 
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
@@ -10,19 +10,32 @@ import "@typechain/hardhat";
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
-    version: "0.8.0",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
+    compilers: [
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
       },
-    },
+      {
+        version: "0.6.11",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    ],
   },
   networks: {
     mumbai: {
       url: process.env.MUMBAI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY || ""],
-    },
+    } as HttpNetworkConfig,
   },
   typechain: {
     outDir: "typechain",
