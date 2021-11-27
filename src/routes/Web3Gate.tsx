37fe174dee9b13eb271dbefc7d98158de86c1d36
Jof <github@jofarnold.com>
2021-11-27 11:49:43 +0000

    Improve loading screen

--- src/routes/Web3Gate.tsx
+++ src/routes/Web3Gate.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { ReactNode, useMemo } from "react";
 import { useQuerySigner } from "../api/useQuerySigner";
 import { WalletContext } from "../providers/WalletProvider";
+import { GameViewPort } from "../Maze/EnvironmentTextures";
+import { AbsoluteCenterFill } from "../components/Layout";
 
 export const Web3Gate = ({ children }: { children: ReactNode }) => {
   const { data: signer } = useQuerySigner();
@@ -11,7 +13,11 @@ export const Web3Gate = ({ children }: { children: ReactNode }) => {
     };
   }, [signer]);
   if (!signer) {
-    return <div>Loading...</div>;
+    return (
+      <GameViewPort>
+        <AbsoluteCenterFill>Loading...</AbsoluteCenterFill>
+      </GameViewPort>
+    );
   }
   return (
     <WalletContext.Provider value={value}>{children}</WalletContext.Provider>
